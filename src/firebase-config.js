5c2b939cfbecdf7ada38072dbc30b4b66281b4f9
Ritik Sachan <sachanritik1@gmail.com>
2023-05-13 22:31:33 +0530

    updated firebase configuration to ritik

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,23 +5,23 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyB2N6yqEtURGwi7mCC2otC7UfrNTxpX9dY",
-	authDomain: "blogapp-22c9d.firebaseapp.com",
-	projectId: "blogapp-22c9d",
-	storageBucket: "blogapp-22c9d.appspot.com",
-	messagingSenderId: "720366436567",
-	appId: "1:720366436567:web:6674442f5a5bb36cf63092",
-}
 // const firebaseConfig = {
-// 	apiKey: "AIzaSyAblPQbYDsXCPUBXPAM_D26PU1xgXnO4Gk",
-// 	authDomain: "blogs-shristi.firebaseapp.com",
-// 	projectId: "blogs-shristi",
-// 	storageBucket: "blogs-shristi.appspot.com",
-// 	messagingSenderId: "478260543972",
-// 	appId: "1:478260543972:web:a98da49cb87e9c596787f1",
-// 	measurementId: "G-24ME39CT6K",
+// 	apiKey: "AIzaSyB2N6yqEtURGwi7mCC2otC7UfrNTxpX9dY",
+// 	authDomain: "blogapp-22c9d.firebaseapp.com",
+// 	projectId: "blogapp-22c9d",
+// 	storageBucket: "blogapp-22c9d.appspot.com",
+// 	messagingSenderId: "720366436567",
+// 	appId: "1:720366436567:web:6674442f5a5bb36cf63092",
 // }
+const firebaseConfig = {
+	apiKey: "AIzaSyAblPQbYDsXCPUBXPAM_D26PU1xgXnO4Gk",
+	authDomain: "blogs-shristi.firebaseapp.com",
+	projectId: "blogs-shristi",
+	storageBucket: "blogs-shristi.appspot.com",
+	messagingSenderId: "478260543972",
+	appId: "1:478260543972:web:a98da49cb87e9c596787f1",
+	measurementId: "G-24ME39CT6K",
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
